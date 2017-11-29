9cd37e5caa3726c26eeeb871122d9cba3e99c240
James Grimsdale <jim6788@gmail.com>
2017-11-29 18:13:57 +0000

    added keys

---
+++ push-keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  GCMAPIKey: "AAAAPJLXYlU:APA91bE9KbEiPOKhocfPFnT3fy1C1uwMkEYAl7CQ4Z8ooK-jGlB4Z7r4eTg656J5xY_qBA3sENv25nh8TtDspVd-RmpX-fb2SIRAuwvRQQSl89vPAdk9vyzmQGr6Ku5IKehuP1yzJacV",
+  subject: "mailto:nodecryptomailer@gmail.com",
+  publicKey: "BCcrFEof9QDSQGO77EIr_83PAZiA2KMMlOmQfs66ibTlUm7rlZPjul2G5DCzpjtGt8ojNJlrPm2_EBlxy9CAwjE",
+  privateKey: "LF7WAQE915strXI-GTuRwAzTE0R8y2yiz0o0uOk-5sg"
+};
